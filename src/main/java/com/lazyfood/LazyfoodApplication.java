d8fc7387ea4c19938cf5f345807c6ad390d9ec0f
vetty <chiv.vetty@gmail.com>
2018-07-22 21:03:53 +0700

    first-commit

---
+++ src/main/java/com/lazyfood/LazyfoodApplication.java
@@ -0,0 +1,12 @@
+package com.lazyfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyfoodApplication.class, args);
+	}
+}
